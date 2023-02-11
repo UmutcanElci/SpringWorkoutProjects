908ad115d7e024d62f278a6da5b436217fcf74ac
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-11 16:23:06 +0300

    StudentManagement

---
+++ BegginerStudentManagementSystem/src/test/java/com/StudentManagement/BeginnerStudentManagement/BeginnerStudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentManagement.BeginnerStudentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerStudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
