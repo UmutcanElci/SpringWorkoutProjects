51d4b84d77fdb8079a46ebdbf4f37b4a92e89425
Umutcan Elci <umutcanelci.me@gmail.com>
2023-02-12 16:53:26 +0300

    WebAppWorkout

---
+++ WebAppWork/src/test/java/com/SpringWorkout/WebAppWork/WebAppWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringWorkout.WebAppWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
