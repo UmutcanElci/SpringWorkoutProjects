fd4a16d4fba845d1c2e1a06a478746ab3eee5e98
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-13 19:00:26 +0300

    BillSplitter-Simple-Project

---
+++ SpringLearn/src/main/java/com/BillSpitter/SpringLearn/Controller/BillSplitterController.java
@@ -0,0 +1,10 @@
+package com.BillSpitter.SpringLearn.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BillSplitterController {
+    @GetMapping("/hello")
+    public String
+}
