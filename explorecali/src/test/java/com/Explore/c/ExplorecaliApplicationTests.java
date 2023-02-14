5d8eb3eeb51554bc73f50536bc830d135834ef39
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-14 17:04:13 +0300

    LinkedinMicroService-Tutorial

---
+++ explorecali/src/test/java/com/Explore/c/ExplorecaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Explore.c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
