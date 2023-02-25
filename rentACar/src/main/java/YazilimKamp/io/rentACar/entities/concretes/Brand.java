3accbdfb8eac6903be89b6e67e9fa62383efc330
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-25 12:41:01 +0300

    Entites-Model

---
+++ rentACar/src/main/java/YazilimKamp/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,31 @@
+package YazilimKamp.io.rentACar.entities.concretes;
+
+public class Brand {
+    //Entities çoğunlukla veri tabanında oluşturduğumuz nesneler diyebiliriz
+    private int id;
+    private String name;
+
+    public Brand() {
+    }
+
+    public Brand(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
