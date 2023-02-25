4d3f64c664446dc63e1b01916bfd4021d58df121
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-25 11:54:44 +0300

    Kamp

---
+++ rentACar/src/test/java/Yaz/l/mKamp/io/rentACar/RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package Yaz.l.mKamp.io.rentACar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
