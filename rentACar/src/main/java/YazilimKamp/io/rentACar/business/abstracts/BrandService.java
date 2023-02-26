3bfc7057c9bba280d78a87613cba94f96689c11f
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-26 16:46:53 +0300

    .

--- rentACar/src/main/java/YazilimKamp/io/rentACar/business/abstracts/BrandService.java
+++
@@ -1,8 +0,0 @@
-package YazilimKamp.io.rentACar.business.abstracts;
-import YazilimKamp.io.rentACar.entities.concretes.Brand;
-
-import java.util.List;
-
-public interface BrandService {
-    List<Brand> getAll();
-}
