fcfc1b703440cb30e69c42d3a787bdff0adbbb3a
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-26 12:32:51 +0300

    Data - Entities with Lombok

--- rentACar/src/main/java/YazilimKamp/io/rentACar/entities/concretes/Brand.java
+++ rentACar/src/main/java/YazilimKamp/io/rentACar/entities/concretes/Brand.java
@@ -1,31 +1,19 @@
 package YazilimKamp.io.rentACar.entities.concretes;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "brands")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Brand {
     //Entities çoğunlukla veri tabanında oluşturduğumuz nesneler diyebiliriz
     private int id;
     private String name;
 
-    public Brand() {
-    }
-
-    public Brand(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
