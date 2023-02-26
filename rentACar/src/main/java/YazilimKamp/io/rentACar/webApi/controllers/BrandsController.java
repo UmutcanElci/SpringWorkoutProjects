869491feae62b4db2f9e58e4cdac0825d60506a5
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-26 12:32:11 +0300

    Web API - Controllers

--- rentACar/src/main/java/YazilimKamp/io/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/YazilimKamp/io/rentACar/webApi/controllers/BrandsController.java
@@ -2,11 +2,12 @@ package YazilimKamp.io.rentACar.webApi.controllers;
 
 import YazilimKamp.io.rentACar.business.abstracts.BrandService;
 import YazilimKamp.io.rentACar.entities.concretes.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.plaf.PanelUI;
+
 import java.util.List;
 
 @RestController//Annotation  spring framework uygulamamız çalıştığında kimde controller olduğunu buluyor ve anlıyorki burası erişim noktası
@@ -14,15 +15,20 @@ import java.util.List;
 public class BrandsController {
     private BrandService brandService;
 
+    @Autowired
     public BrandsController(BrandService brandService) {
         this.brandService = brandService;
     }
+    //anlamı şu an BrandService olduğu için autowired parametrelerine bakıyor uygulamayı tarar ve implimitasyonları bulur ve onların new lenmiş halini verir
+    //yani burda IoC kullanıyoruz bunun amacı biz sürekli bir object oluşturmak yerine her yeni veri için , onun yerine bellek te tek obje oluşturarak hepsini o
+    //obje üzerinden dönyor(Kaba bir açıklama!)
 
-    @GetMapping("/getAll")
+    @GetMapping("/getall")
     public List<Brand> getAll(){
         return brandService.getAll();
     }
     /*
-    Şimdi biz /api/brands/getAll adresine istek attık
+    Şimdi biz /api/brands/getAll adresine istek attık bunun için brandService yani busness a gidicez, oda iş kurallarını çalıştıracak
+    ordan da repository e gidice ve veriyi bize vericek
      */
 }
