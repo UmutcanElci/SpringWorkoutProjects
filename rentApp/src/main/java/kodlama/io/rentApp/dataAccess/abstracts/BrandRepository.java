28212cd376e77d7da1a90c89763566952386228a
Umutcan Elci <umutcanelci.me@gmail.com>
2023-02-26 19:54:26 +0300

    Data Access - Repository

---
+++ rentApp/src/main/java/kodlama/io/rentApp/dataAccess/abstracts/BrandRepository.java
@@ -0,0 +1,11 @@
+package kodlama.io.rentApp.dataAccess.abstracts;
+
+import kodlama.io.rentApp.entities.concretes.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface BrandRepository extends JpaRepository<Brand,Integer> {//Integer veri tipi (ID ya da primary key de diyebiliriz)
+    //BrandDao da denir
+}
