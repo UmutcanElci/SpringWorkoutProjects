d5e3e3c8511ecf90c5ae7b27f84e288570669bfa
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-28 02:50:25 +0300

    Service - Business

--- rentApp/src/main/java/kodlama/io/rentApp/business/abstracts/BrandService.java
+++ rentApp/src/main/java/kodlama/io/rentApp/business/abstracts/BrandService.java
@@ -1,10 +1,17 @@
 package kodlama.io.rentApp.business.abstracts;
 
-import kodlama.io.rentApp.entities.concretes.Brand;
-import org.springframework.stereotype.Service;
+import kodlama.io.rentApp.business.requests.CreateBrandRequest;
+import kodlama.io.rentApp.business.requests.UpdateBrandRequest;
+import kodlama.io.rentApp.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentApp.business.responses.GetByIdBrandResponse;
 
 import java.util.List;
 
-public interface BrandService {
-    List<Brand> getall();
+public interface BrandService {//Veri erişim işlemleri burada gerçekleşir
+    List<GetAllBrandsResponse> getAll();
+    GetByIdBrandResponse getById(int id);
+    void add(CreateBrandRequest request);
+
+    void update(UpdateBrandRequest update);
+    void delete(int id);
 }
