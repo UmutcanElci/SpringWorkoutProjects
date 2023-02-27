357db13de88cbabb0d696a9d1e342c756c5eaa39
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-28 02:53:14 +0300

    Model Mapper Manager

--- rentApp/src/main/java/kodlama/io/rentApp/core/utilities/mappers/ModelMapperManager.java
+++ rentApp/src/main/java/kodlama/io/rentApp/core/utilities/mappers/ModelMapperManager.java
@@ -5,8 +5,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
-@Service
 @AllArgsConstructor
+@Service
 public class ModelMapperManager implements ModelMapperService {
 
     private ModelMapper modelMapper;
