5c82a18b93c3e9437b4647ea09d437258b501588
Umutcan Elci <umutcanelci.me@gmail.com>
2023-02-28 02:49:30 +0300

    Mapper Added

--- rentApp/src/main/java/kodlama/io/rentApp/RentAppApplication.java
+++ rentApp/src/main/java/kodlama/io/rentApp/RentAppApplication.java
@@ -1,7 +1,10 @@
 package kodlama.io.rentApp;
 
+import kodlama.io.rentApp.core.utilities.mappers.ModelMapperManager;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RentAppApplication {
@@ -10,4 +13,8 @@ public class RentAppApplication {
 		SpringApplication.run(RentAppApplication.class, args);
 	}
 
+	@Bean//Bu bir nesne ihtiyaç duyulacak diye spring bunu uygulamamızın içine ekler
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
+	}
 }
