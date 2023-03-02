6e6da10798641d245c7a70254ab00c46e89428be
Umutcan Elçi <umutcanelci@yaani.com>
2023-03-02 20:20:08 +0300

    Model Controller

---
+++ rentApp/src/main/java/kodlama/io/rentApp/webApi/controllers/ModelsController.java
@@ -0,0 +1,29 @@
+package kodlama.io.rentApp.webApi.controllers;
+
+import jakarta.validation.Valid;
+import kodlama.io.rentApp.business.abstracts.ModelService;
+import kodlama.io.rentApp.business.requests.CreateModelRequest;
+import kodlama.io.rentApp.business.responses.GetAllModelsResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/models")
+@AllArgsConstructor
+public class ModelsController {
+    private ModelService modelService;
+    @GetMapping()
+    public List<GetAllModelsResponse> getAll(){
+        return modelService.getAll();
+    }
+
+    @PostMapping()
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void add(@RequestBody() @Valid() CreateModelRequest createModelRequest){
+        this.modelService.add(createModelRequest);
+    }
+
+}
