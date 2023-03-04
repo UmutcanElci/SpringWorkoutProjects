2816b66ac5d3e5d1dcf328ea8cad7dab18cac358
Umutcan Elci <umutcanelci.me@gmail.com>
2023-03-04 18:59:32 +0300

    KampOdev

---
+++ KodlamaOdev/src/main/java/Kodlama/io/Devs/KodlamaOdev/KodlamaOdevApplication.java
@@ -0,0 +1,13 @@
+package Kodlama.io.Devs.KodlamaOdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodlamaOdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodlamaOdevApplication.class, args);
+	}
+
+}
---
+++ KodlamaOdev/src/test/java/Kodlama/io/Devs/KodlamaOdev/KodlamaOdevApplicationTests.java
@@ -0,0 +1,13 @@
+package Kodlama.io.Devs.KodlamaOdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodlamaOdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
